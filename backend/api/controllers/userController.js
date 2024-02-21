0d5893bbd7bbe85e42a0c9fe81ef5219239e65aa
Meet <proobmeet7834@gmail.com>
2024-02-21 20:46:30 +0530

    structured files

---
+++ backend/api/controllers/userController.js
@@ -0,0 +1,46 @@
+const User = require('../../models/UserModel');
+
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.createUser = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.updateUser = async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.userId);
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(204).end();
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 
-const userRoutes = require('./Routes/userRoutes');
-const teamRoutes = require('./Routes/teamRoutes');
-const matchRoutes = require('./Routes/matchRoutes');
-const scorecardRoutes = require('./Routes/scorecardRoutes');
+const userRoutes = require('./api/routes/userRoutes');
+const teamRoutes = require('./api/routes/teamRoutes');
+const matchRoutes = require('./api/routes/matchRoutes');
+const scorecardRoutes = require('./api/routes/scorecardRoutes');
 
 dotenv.config();
 const app = express();
@@ -20,14 +20,20 @@ mongoose.connect(MONGO_URL)
 app.use(express.json());
 app.use(morgan('tiny'));
 
-app.use('/', userRoutes);
-app.use('/', teamRoutes);
-app.use('/', matchRoutes);
-app.use('/', scorecardRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/team', teamRoutes);
+app.use('/api/match', matchRoutes);
+app.use('/api/scorecard', scorecardRoutes);
 
 app.get('/', (req,res) => {
     res.send("Hello");
 })
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal server error' });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
